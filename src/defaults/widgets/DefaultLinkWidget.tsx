6ba103b689cf7222dfba68e8a1c736539ac46fdf
Jonathan Sweetman <jonathan.sweetman@toptal.com>
2018-10-10 16:26:48 -0400

    right clicking link no longer deletes node

--- src/defaults/widgets/DefaultLinkWidget.tsx
+++ src/defaults/widgets/DefaultLinkWidget.tsx
@@ -166,10 +166,10 @@ export class DefaultLinkWidget extends BaseWidget<DefaultLinkProps, DefaultLinkS
 			strokeOpacity: this.state.selected ? 0.1 : 0,
 			strokeWidth: 20,
 			onContextMenu: () => {
-				if (!this.props.diagramEngine.isModelLocked(this.props.link)) {
-					event.preventDefault();
-					this.props.link.remove();
-				}
+				// if (!this.props.diagramEngine.isModelLocked(this.props.link)) {
+				// 	event.preventDefault();
+				// 	this.props.link.remove();
+				// }
 			}
 		});
 
