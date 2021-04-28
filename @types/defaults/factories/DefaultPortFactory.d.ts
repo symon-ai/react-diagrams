3fae2b6f2b5ad0c4972f6be6401e35a21dbd5290
Jonathan Sweetman <jonathansweetman@Jonathan-Sweetmans-MacBook-Pro.local>
2021-04-28 09:36:14 -0400

    remove types folder

--- @types/defaults/factories/DefaultPortFactory.d.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultPortModel } from "../models/DefaultPortModel";
-import { AbstractPortFactory } from "../../factories/AbstractPortFactory";
-export declare class DefaultPortFactory extends AbstractPortFactory<DefaultPortModel> {
-    constructor();
-    getNewInstance(initialConfig?: any): DefaultPortModel;
-}
