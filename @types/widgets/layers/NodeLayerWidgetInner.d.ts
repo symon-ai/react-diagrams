20896a24469d5d6d41207202f2280f14aa69befb
Jonathan Sweetman <jonathansweetman@Jonathan-Sweetmans-MacBook-Pro.local>
2021-05-31 08:39:20 -0400

    removed unused folder

--- @types/widgets/layers/NodeLayerWidgetInner.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { DiagramEngine } from "../../DiagramEngine";
-export interface NLWIProps {
-    diagramEngine: DiagramEngine;
-}
-export declare class NodeLayerWidgetInner extends React.Component<NLWIProps> {
-    shouldComponentUpdate(nextProps: any): boolean;
-    render(): JSX.Element;
-}
