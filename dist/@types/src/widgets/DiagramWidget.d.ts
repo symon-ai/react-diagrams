e44f7c52a05cddf47773ab3e3fe910a9a9095a2a
jsweetman <60225114+jsweetman@users.noreply.github.com>
2021-05-31 08:40:14 -0400

    Fix dup links going backwards (#9)
    * fix dbackwards dup links
    
    * removed unused folder
    
    Co-authored-by: Jonathan Sweetman <jonathansweetman@Jonathan-Sweetmans-MacBook-Pro.local>

--- dist/@types/src/widgets/DiagramWidget.d.ts
+++ dist/@types/src/widgets/DiagramWidget.d.ts
@@ -7,59 +7,75 @@ import { LinkModel } from "../models/LinkModel";
 import { PortModel } from "../models/PortModel";
 import { BaseWidget, BaseWidgetProps } from "./BaseWidget";
 export interface DiagramProps extends BaseWidgetProps {
-    diagramEngine: DiagramEngine;
-    allowLooseLinks?: boolean;
-    allowCanvasTranslation?: boolean;
-    allowCanvasZoom?: boolean;
-    inverseZoom?: boolean;
-    maxNumberPointsPerLink?: number;
-    smartRouting?: boolean;
-    disableNodeMove?: boolean;
-    actionStartedFiring?: (action: BaseAction) => boolean;
-    actionStillFiring?: (action: BaseAction) => void;
-    actionStoppedFiring?: (action: BaseAction) => void;
-    deleteKeys?: number[];
-    onMoveStart?: (action: MoveItemsAction) => void;
-    onMovingSingle?: (action: MoveItemsAction) => void;
-    onMovingSingle2?: (action: MoveItemsAction) => void;
-    onMoveFinished?: (action: MoveItemsAction, nodeLink?: any) => void;
-    blockDelete?: boolean;
+	diagramEngine: DiagramEngine;
+	allowLooseLinks?: boolean;
+	allowCanvasTranslation?: boolean;
+	allowCanvasZoom?: boolean;
+	inverseZoom?: boolean;
+	maxNumberPointsPerLink?: number;
+	smartRouting?: boolean;
+	disableNodeMove?: boolean;
+	actionStartedFiring?: (action: BaseAction) => boolean;
+	actionStillFiring?: (action: BaseAction) => void;
+	actionStoppedFiring?: (action: BaseAction) => void;
+	deleteKeys?: number[];
+	onMoveStart?: (action: MoveItemsAction) => void;
+	onMovingSingle?: (action: MoveItemsAction) => void;
+	onMovingSingle2?: (action: MoveItemsAction) => void;
+	onMoveFinished?: (action: MoveItemsAction, nodeLink?: any) => void;
+	shouldAddLink?: (
+		pointLink: LinkModel,
+		srcPort: PortModel,
+		tarPort: PortModel
+	) => boolean;
+	blockDelete?: boolean;
 }
 export interface DiagramState {
-    action: BaseAction | null;
-    wasMoved: boolean;
-    renderedNodes: boolean;
-    windowListener: any;
-    diagramEngineListener: any;
-    document: any;
+	action: BaseAction | null;
+	wasMoved: boolean;
+	renderedNodes: boolean;
+	windowListener: any;
+	diagramEngineListener: any;
+	document: any;
 }
 /**
  * @author Dylan Vorster
  */
-export declare class DiagramWidget extends BaseWidget<DiagramProps, DiagramState> {
-    static defaultProps: DiagramProps;
-    onKeyUpPointer: (this: Window, ev: KeyboardEvent) => void;
-    constructor(props: DiagramProps);
-    componentWillUnmount(): void;
-    componentWillReceiveProps(nextProps: DiagramProps): void;
-    componentWillUpdate(nextProps: DiagramProps): void;
-    componentDidUpdate(): void;
-    componentDidMount(): void;
-    /**
-     * Gets a model and element under the mouse cursor
-     */
-    getMouseElement(event: any): {
-        model: BaseModel<BaseEntity, BaseModelListener>;
-        element: Element;
-    };
-    fireAction(): void;
-    stopFiringAction(shouldSkipEvent?: boolean): void;
-    startFiringAction(action: BaseAction): void;
-    onMouseMove(event: any): void;
-    onKeyUp(event: any): void;
-    graphAlreadyHasLink(pointLink: LinkModel, diagramLinks: any, element: any): boolean;
-    shouldAddLink(pointLink: LinkModel, srcPort: PortModel, tarPort: PortModel): boolean;
-    onMouseUp(event: any): void;
-    drawSelectionBox(): JSX.Element;
-    render(): JSX.Element;
+export declare class DiagramWidget extends BaseWidget<
+	DiagramProps,
+	DiagramState
+> {
+	static defaultProps: DiagramProps;
+	onKeyUpPointer: (this: Window, ev: KeyboardEvent) => void;
+	constructor(props: DiagramProps);
+	componentWillUnmount(): void;
+	componentWillReceiveProps(nextProps: DiagramProps): void;
+	componentWillUpdate(nextProps: DiagramProps): void;
+	componentDidUpdate(): void;
+	componentDidMount(): void;
+	/**
+	 * Gets a model and element under the mouse cursor
+	 */
+	getMouseElement(event: any): {
+		model: BaseModel<BaseEntity, BaseModelListener>;
+		element: Element;
+	};
+	fireAction(): void;
+	stopFiringAction(shouldSkipEvent?: boolean): void;
+	startFiringAction(action: BaseAction): void;
+	onMouseMove(event: any): void;
+	onKeyUp(event: any): void;
+	graphAlreadyHasLink(
+		pointLink: LinkModel,
+		diagramLinks: any,
+		element: any
+	): boolean;
+	shouldAddLink(
+		pointLink: LinkModel,
+		srcPort: PortModel,
+		tarPort: PortModel
+	): boolean;
+	onMouseUp(event: any): void;
+	drawSelectionBox(): JSX.Element;
+	render(): JSX.Element;
 }
