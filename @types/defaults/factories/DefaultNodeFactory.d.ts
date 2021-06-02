695f92daef713ff199dc8620c6e19d3a661c3ea9
jsweetman <60225114+jsweetman@users.noreply.github.com>
2021-06-02 08:27:02 -0400

    move link removal into symon (#10)
    Co-authored-by: Jonathan Sweetman <jonathansweetman@Jonathan-Sweetmans-MacBook-Pro.local>

---
+++ @types/defaults/factories/DefaultNodeFactory.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="react" />
+import { DefaultNodeModel } from "../models/DefaultNodeModel";
+import { DiagramEngine } from "../../DiagramEngine";
+import { AbstractNodeFactory } from "../../factories/AbstractNodeFactory";
+/**
+ * @author Dylan Vorster
+ */
+export declare class DefaultNodeFactory extends AbstractNodeFactory<DefaultNodeModel> {
+    constructor();
+    generateReactWidget(diagramEngine: DiagramEngine, node: DefaultNodeModel): JSX.Element;
+    getNewInstance(initialConfig?: any): DefaultNodeModel;
+}
